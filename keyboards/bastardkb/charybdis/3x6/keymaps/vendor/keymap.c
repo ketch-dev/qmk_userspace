405ce06cbe80d6df9ce5eb1ee545b396c6aac8b4
Ilya Marchik <ketch.dev@gmail.com>
2025-04-01 13:16:06 +0200

    replace cyrillic characters with similar english

--- keyboards/bastardkb/charybdis/3x6/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/vendor/keymap.c
@@ -29,14 +29,14 @@ enum custom_keycodes {
 };
 
 enum tap_dance {
-    TD_Э_Ё,
-    TD_Х_Ъ,
+    TD_3_E,
+    TD_X_b,
     TD_MS_4_5,
 };
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
-        TD(TD_Э_Ё) ,KC_Q    ,KC_W    ,KC_E     ,KC_R           ,KC_T          ,                /**/                   KC_Y   ,KC_U ,KC_I    ,KC_O   ,KC_P              ,TD(TD_Х_Ъ) ,
+        TD(TD_3_E) ,KC_Q    ,KC_W    ,KC_E     ,KC_R           ,KC_T          ,                /**/                   KC_Y   ,KC_U ,KC_I    ,KC_O   ,KC_P              ,TD(TD_X_b) ,
         KC_ESC     ,KC_A    ,KC_S    ,KC_D     ,KC_F           ,KC_G          ,                /**/                   KC_H   ,KC_J ,KC_K    ,KC_L   ,KC_SCLN           ,KC_ENT     ,
         KC_LGUI    ,KC_Z    ,KC_X    ,KC_C     ,KC_V           ,KC_B          ,                /**/                   KC_N   ,KC_M ,KC_COMM ,KC_DOT ,LT(_MNG, KC_SLSH) ,KC_LALT    ,
                                                 KC_MT_CTRL_MEH ,KC_LT_SYS_CSG ,KC_MT_LSFT_CS , /**/ LT(_SYM, KC_TAB) ,KC_SPC
@@ -173,7 +173,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 tap_dance_action_t tap_dance_actions[] = {
-    [TD_Э_Ё]    = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_GRV),
-    [TD_Х_Ъ]    = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC),
+    [TD_3_E]    = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_GRV),
+    [TD_X_b]    = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC),
     [TD_MS_4_5] = ACTION_TAP_DANCE_DOUBLE(MS_BTN4, MS_BTN5),
 };
